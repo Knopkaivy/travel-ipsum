b221dadafcc713a382cb6933f141238404e79d7c
Knopka <knopkaivy@gmial.com>
2019-06-09 10:12:03 -0700

    Transfer Image component into functional from class based

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,27 +1,12 @@
 import React, { Component } from 'react';
-import { Transition } from 'react-transition-group';
 
-class Image extends Component {
-	constructor(props) {
-		super(props);
-	}
-
-	componentDidMount() {
-		// console.log('image component did mount');
-	}
-
-	componentWillUnmount() {
-		// console.log('image component will unmount');
-	}
-
-	render() {
-		return (
-			<div
-				className='App-image-container'
-				style={{ background: `url(${this.props.imageURL}) bottom center / cover no-repeat` }}
-			/>
-		);
-	}
-}
+const Image = (props) => {
+	return (
+		<div
+			className='App-image-container'
+			style={{ background: `url(${props.imageURL}) bottom center / cover no-repeat` }}
+		/>
+	);
+};
 
 export default Image;
