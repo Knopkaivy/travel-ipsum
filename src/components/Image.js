5e631b341dee431749b91422744f3c0a375a2bae
Knopka <knopkaivy@gmial.com>
2019-06-10 20:04:36 -0700

    Add <img/> instead of <div/> background-image

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,15 +1,12 @@
 import React from 'react';
-// import { CSSTransition } from 'react-transition-group';
-// import '../styles/styles.css';
 
 const Image = (props) => {
 	return (
-		// <CSSTransition key={props.key} timeout={500} classNames='move'>
-		<div
-			className='App-image-container'
-			style={{ background: `url(${props.imageURL}) bottom center / cover no-repeat` }}
-		/>
-		// </CSSTransition>
+		<div className='App-image-container'>
+			{/* <picture> */}
+			<img src={props.imageURL} alt={props.active} />
+			{/* </picture> */}
+		</div>
 	);
 };
 
