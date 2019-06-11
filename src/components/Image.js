d77a3ff8cb91c349e4312f0c4221a646f2d1ffbe
Knopka <knopkaivy@gmial.com>
2019-06-10 20:06:54 -0700

    Remove <picture/>

--- src/components/Image.js
+++ src/components/Image.js
@@ -3,9 +3,7 @@ import React from 'react';
 const Image = (props) => {
 	return (
 		<div className='App-image-container'>
-			{/* <picture> */}
 			<img src={props.imageURL} alt={props.active} />
-			{/* </picture> */}
 		</div>
 	);
 };
